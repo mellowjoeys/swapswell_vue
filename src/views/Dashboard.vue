9ac10dd3a884a2a0084f0053fc92140e8ce87e3c
brittani <britt.sentiere@gmail.com>
2019-10-22 19:39:33 -0500

    updated dashboard vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,45 +1,18 @@
 <template>
   <div class="dashboard">
     <h1>Dashboard</h1>
-    <swipe ref="mySwipe" style='max-width:500px;margin:0 auto'>
-      
-      <div v-for="good in goods">
-        <swipe-item>
-          <!-- {{ goods }} -->
-          <b>1</b>
-          <!-- <router-link v-bind:to="'/goods/' + good.id">
-            <img v-bind:src="good.image_url" v-bind:alt="good.name">
-          </router-link> -->
-        </swipe-item>
-      </div>
-    </swipe>
+    
+    <div v-for="photo in photos"> 
+      {{ photo.image_url }}
+    </div>
 
-
-        <!-- <div v-for="product in products">
-              <h2>{{ product.name }}</h2>
-              <router-link v-bind:to="'/products/' + product.id">
-                <img v-bind:src="product.image_url" v-bind:alt="product.name">
-              </router-link> -->
-    <button @click="next" class="btn btn-default">Previous</button>
-    <button @click="prev" class="btn btn-default">Next</button>
+    <button v-on:click="want()" class="btn btn-default">Want!</button>
+    <button v-on:click="noThanks()" class="btn btn-default">No Thanks</button>
   </div>
 </template>
 
 <style>
-/*.swipe {
-  overflow: hidden;
-  visibility: hidden;
-  position: relative;
-}
-.swipe-wrap {
-  overflow: hidden;
-  position: relative;
-}
-.swipe-wrap > div {
-  float: left;
-  width: 100%;
-  position: relative;
-}*/
+
 </style>
 
 <script>
@@ -47,32 +20,26 @@ var axios = require('axios');
 export default {
   data: function() {
     return {
-      swipeOptions: {
-        startSlide: 0,
-        speed: 300,
-        auto: 4000,
-        continuous: true,
-        disableScroll: false,
-        stopPropagation: false
-      },
-      goods: []
+     
+      photos: []
     };
 
   },
   created: function() {
     axios
-      .get("/api/goods")
+      .get("/api/photos/others")
       .then(response => {
-        this.goods = response.data;
+        this.photos = response.data;
       });
   },
   methods: {
-    prev() {
-      this.$refs.mySwipe.prev()
+    want() {
+     
     },
-    next() {
-      this.$refs.mySwipe.next()
+    noThanks() {
+     
     }
   }
 };
 </script>
+
